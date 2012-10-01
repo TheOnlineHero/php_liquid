733038c0d2af4d91357e70dc6ceca156df28b8e1
TheOnlineHero <TheOnlineHero2012@gmail.com>
2012-10-01 14:38:06 +0800

    Added comments.

--- php_liquid.php
+++ php_liquid.php
@@ -1,27 +1,37 @@
 <?php
 /*
 Plugin Name: Liquid PHP
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: Allows you to add {{site_url}} liquid tag into your stylesheets and posts to print your sites url. 
-Use liquid_stylesheet_url() in your header.php template instead of bloginfo( 'stylesheet_url' ) to parse your stylesheet.
+Plugin URI: https://github.com/TheOnlineHero/php_liquid/zipball/master
+Description: PHP Liquid is a WordPress plugin that allows you to add liquid tags
+to your stylesheet and page/posts.
 
-When you want to use site_url in your stylesheet or post, use it like this:
-Example:
-body {
-  background: url({{site_url}}/test_image.png)
-}
+Installation:
+
+1) Install WordPress 3.4.2 or higher
+
+2) Download the following file:
+https://github.com/TheOnlineHero/php_liquid/zipball/master
+
+3) Login to WordPress admin, click on Plugins / Add New / Upload, then upload the zip file you just downloaded.
+
+4) Activate the plugin.
+
+5) Edit your header.php and find bloginfo( 'stylesheet_url' ). Replace this with liquid_stylesheet_url().
+
+6) Thats it, but if you want your own liquid tags, in your functions.php file write the following method
+function override_mytheme_liquid_array() { 	
+  // Declare your liquid tags in this array.	
+  return array('site_url' => get_option('siteurl'), "dude" => "TheOnlineHero"); 
+  // Then in your post/page or stylesheet type {{site_url}}, {{dude}}, to use them.
+  // Example:
+  // body {
+  //   background: url({{site_url}}/images/test_image.png);
+  // }
+} 
 
 
-Please use the override_mytheme_liquid_array() function to override the liquid variables that you want in your stylesheets and posts.
-Example:
-function override_mytheme_liquid_array() {
-		return array('site_url' => get_option('siteurl'), "dude" => "tom");
-}
-Best place to write this method is in your themes functions.php template.
-
-
-Version: 1.0
-Author: Tom Skroza
+Version: 1.1
+Author: TheOnlineHero
 Author URI: 
 License: GPL2
 */
